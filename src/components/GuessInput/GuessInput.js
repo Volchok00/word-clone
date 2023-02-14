1f02dfe7c6953d880188bba2eed1a008dd6f19f5
volchok00 <katsiaryna.gatti@gmail.com>
2023-02-14 12:10:05 +0100

    fix: console errors

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,7 @@
 export const GuessInput = ({ guess, onChange, onSubmit }) => {
   return (
-    <form class="guess-input-wrapper" onSubmit={onSubmit}>
-      <label for="guess-input">Enter guess:</label>
+    <form className="guess-input-wrapper" onSubmit={onSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
       <input
         id="guess-input"
         type="text"
