ef5f096b25cb104e1422bd7aabfbf8da1d5207b4
volchok00 <katsiaryna.gatti@gmail.com>
2023-02-14 11:50:16 +0100

    feat: add basic GuessInput to the Game

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,7 @@
+export const GuessInput=()=> {
+    return <form class="guess-input-wrapper">
+    <label for="guess-input">Enter guess:</label>
+    <input id="guess-input" type="text" />
+  </form>
+}
+
