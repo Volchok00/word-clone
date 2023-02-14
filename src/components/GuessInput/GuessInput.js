a03f3fa1812405531889f9527023fbef7beeaf11
volchok00 <katsiaryna.gatti@gmail.com>
2023-02-14 12:13:05 +0100

    refactor: onChange

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,12 +2,7 @@ export const GuessInput = ({ guess, onChange, onSubmit }) => {
   return (
     <form className="guess-input-wrapper" onSubmit={onSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
-      <input
-        id="guess-input"
-        type="text"
-        value={guess}
-        onChange={(e) => onChange(e)}
-      />
+      <input id="guess-input" type="text" value={guess} onChange={onChange} />
     </form>
   );
 };
